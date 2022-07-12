10524709dbda595d87e141120974a2223eb1bc09
mdooedu <107252964+mdooedu@users.noreply.github.com>
2022-07-13 08:36:28 +0900

    .

--- Location.py
+++ Location.py
@@ -31,7 +31,7 @@ def GetVecVal(count):  # count는 입력 받을 큐브의 전체 갯수
         temp = range(1, count + 1)
         list(temp)
 
-        for i in temp:  # pCube의 좌표값은 번호대호 반환 반복
+        for i in temp:  # pCube의 좌표값을 번호대호 반환 반복
             xLocation.append(cmds.getAttr("pCube" + str(i) + ".translateX"))
             yLocation.append(cmds.getAttr("pCube" + str(i) + ".translateY"))
             zLocation.append(cmds.getAttr("pCube" + str(i) + ".translateZ"))
@@ -59,10 +59,10 @@ def ReturnRange(listofvalran):  # listofvalran은 정렬 된 큐브의 좌표값
 
     for p in range(3):  # x,y,z 리스트의 최소 최대값 반환
 
-        i += 1
+        i += 1  # x, y, z 인덱스 반복 출력을 위한 for문 상수값
 
-        a = listofvalran[p][0]
-        b = listofvalran[p][-1]
+        a = listofvalran[p][0]  # 정렬 된 리스트의 인덱스 0번값; 최소값 a로 반환
+        b = listofvalran[p][-1]  # 정렬된 리스트의 인덱스 -1값; 최대값 b로 반환
 
         if i == 1:
             xRange.append(a)
@@ -85,8 +85,8 @@ def ReturnFinalRnaVal(listreturn, targetcount):  # xyz값을 순서대로 오브
     z = count_gen(listreturn[2][0], listreturn[2][1], targetcount)
 
     for i in range(targetcount):
-        uniDB.append([x[0], y[0], z[0]])
-        x.pop(0)
+        uniDB.append([x[0], y[0], z[0]])  # 좌표값 리스트 0번 인덱스 오브젝트 인덱스 0번으로 입력
+        x.pop(0)  # 입력한 인덱스 값 삭제 추후 pop값 로그로 출력모듈 도입
         y.pop(0)
         z.pop(0)
 
@@ -94,12 +94,13 @@ def ReturnFinalRnaVal(listreturn, targetcount):  # xyz값을 순서대로 오브
 
 
 def mainfuc(a, temp):
+
     b = GetVecVal(a)  # 오브젝트의 좌표값을 호출받음
     c = GetRangeVec(b)  # 입력 받은 값 정렬
     d = ReturnRange(c)  # 정령된 리스트의 최소 최댓값 반환
     asResul = ReturnFinalRnaVal(d, temp)  # 랜덤값 생성 및 반환 후 오브젝트 입력값 순서로 재정렬
 
-    return asResul
+    return asResul  # 최종 범위 내에서 랜덤 생성된 값 x, y, z순으로 인덱스 반환
 
 
 a = int(input("값을 입력받을 큐브의 갯수\n"))
