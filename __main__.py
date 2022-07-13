9f034f30c69de470c7f9cd0b965e9a85c37ce542
mdooedu <107252964+mdooedu@users.noreply.github.com>
2022-07-13 11:05:02 +0900

    .

--- __main__.py
+++ __main__.py
@@ -3,7 +3,10 @@ from MadedFucRe import *
 
 
 a = int(input("값을 입력받을 큐브의 갯수\n"))
-temp = int(input("만들 좌표의 갯수\n"))
+temp = int(input("temp\n"))
 
 print(mainFucLoc(a, temp))  # 랜덤 로케이션 반환값
 print(mainFucScl(temp))  # 랜덤 반환된 사이즈값
+'''
+정상출력 확인 mainFucLoc, mainFucScl
+'''
