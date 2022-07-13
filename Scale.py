db2ba075e2c4436077c13c4d954c8a8d55f865ee
mdooedu <107252964+mdooedu@users.noreply.github.com>
2022-07-13 09:13:24 +0900

    .

--- Scale.py
+++ Scale.py
@@ -16,15 +16,18 @@ def count_gen(a, b, target_count):
 
 
 def GetVecVal():  # frequency는 반환 받은 스케일의 중간값 반환
-
     xyzScale = []
-
     temp = [1, 2]
+    ifc = 0
 
     for i in temp:  # pCube의 좌표값을 번호대호 반환 반복
-        xyzScale.append(list(cmds.getAttr("Scale" + str(i) + ".scale")))  # 튜플로 들어온 좌표값 리스트로 변환후 반환
+        p = cmds.getAttr("Scale" + str(i) + ".scale")  # 좌표값이 튜플로 입력됨
+        print(p)
+        xyzScale.append(list(p[ifc]))  # 튜플로 들어온 좌표값 리스트로 변환후 반환
 
-    return xyzScale
+        ifc += 1
+
+    return xyzScale  # 튜플 자료형 때문에 리스트로 받고 반환됨
 
 
 print(GetVecVal())
