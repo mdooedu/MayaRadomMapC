badc98da9ca7168952205c9e1108f315478b50d1
mdooedu <107252964+mdooedu@users.noreply.github.com>
2022-07-13 11:00:29 +0900

    .

--- MadedFucRe.py
+++ MadedFucRe.py
@@ -99,7 +99,6 @@ def GetVecValScl():  # frequency는 반환 받은 스케일의 중간값 반환
 
     for i in temp:  # pCube의 좌표값을 번호대호 반환 반복
         p = cmds.getAttr("Scale" + str(i) + ".scale")  # 좌표값이 튜플로 입력됨
-        print(p)
         xyzScale.append(list(p[0]))  # 튜플로 들어온 좌표값 리스트로 변환후 반환
 
     return xyzScale  # 리스트 안의 리스트로 반환
